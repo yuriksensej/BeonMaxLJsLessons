4ee87537507b4d37fddecce181514ac43b423af7
yuriksensej <yuraminchenko@gmail.com>
2021-03-06 20:00:13 +0200

    06.03.21

--- js/script.js
+++ js/script.js
@@ -1,46 +1,26 @@
-// Form
-
-let message = {
-  loading: 'Загрузка...',
-  success: 'Спасибо! Скоро мы с вами свяжемся!',
-  failure: 'Что-то пошло не так...',
-};
-
-let form = document.querySelector('.main-form'),
-  input = form.getElementsByTagName('input'),
-  statusMessage = document.createElement('div');
-
-statusMessage.classList.add('status');
-
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  form.appendChild(statusMessage);
-
+let inputRub = document.getElementById('rub'),
+  inputUsd = document.getElementById('usd');
+let count = 1;
+inputRub.addEventListener('input', () => {
+  //let myPromise = new Promise(function (myResolve, myReject) {
+  //
   let request = new XMLHttpRequest();
-  request.open('POST', 'server.php');
-  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-
-  let formData = new FormData(form);
-
-  let obj = {};
-  formData.forEach(function (value, key) {
-    obj[key] = value;
-  });
-  let json = JSON.stringify(obj);
-
-  request.send(json);
-
   request.addEventListener('readystatechange', function () {
-    if (request.readyState < 4) {
-      statusMessage.innerHTML = message.loading;
-    } else if (request.readyState === 4 && request.status == 200) {
-      statusMessage.innerHTML = message.success;
+    // setTimeout(f1, 1000);
+    // function f1() {
+    if (request.readyState === 4 && request.status === 200) {
+      let data = JSON.parse(request.response);
+      inputUsd.value = inputRub.value / data.usd;
+      console.log(
+        inputUsd.value + ' ' + data.usd + ' ' + count + ' ' + request.readyState
+      );
+      count++;
     } else {
-      statusMessage.innerHTML = message.failure;
+      inputUsd.value = 'Что-то пошло не так!';
     }
+    // }
   });
-
-  for (let i = 0; i < input.length; i++) {
-    input[i].value = '';
-  }
+  request.open('GET', 'js/current.json');
+  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  request.send();
 });
